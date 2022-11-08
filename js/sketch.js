ed0df1cb4959c582402fe4eef192cb4c730be403
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-08 10:00:52 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,24 @@
+// Variables List - Top of Code = Global Scope
+var firstName = 'Luke';
+var thisIsAStrig = 'Hello';
+var ThisIsAInteger = 100;
+var ThisIsAnUndefinedVar;
+var thisIsNull = null;
+var ellipseX = 400;
+var ellipseY = 400;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    console.log(ThisIsAInteger);
+    background(220);
 }
 
 function draw() {
-    background(220);
+    
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(mouseX, mouseY, 100);
+
+    console.log(width, height);
 }
